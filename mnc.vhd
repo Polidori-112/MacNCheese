361700ff2c1dc6d5a0a448af20f732e8290ac4fc
Lucas Polidori <92409632+Polidori-112@users.noreply.github.com>
2024-04-17 13:42:58 -0400

    minor changes

--- mnc.vhd
+++ mnc.vhd
@@ -3,32 +3,31 @@ use ieee.std_logic_1164.all;
 use IEEE.numeric_std.all;
 use ieee.std_logic_unsigned.all;
 
-entity top is 
+entity MNC is 
 generic(
-	sample_type : integer := 0;    -- Used to select which sampler is used: 1 for RAW, 0 for RAM
+	use_ram     : integer := 0;    -- Used to select which sampler is used: 0 for Continuous Read, 1 for RAM
 	use_ext_clk : integer := 0;    -- Used to connect sampling rate to defined clock signal
 	                               -- Must additionally connect 'ext_clk' to defined clock
 	byterate    : integer := 1;    -- Used to send byte when high (MUST BE <= 20000) 
 								   -- this is bytes per second transmitted when use_ext_clk = 0
 								   -- Set this value to 1 when use_ext_clk is high
 	NUM_INPUTS  : natural := 8;    -- Number of input bytes (DO NOT CHANGE)
-	NUM_SAMPLES : natural := 1024; -- Maximum number of samples when using RAM (must be < 125000)
-	ADDR_WIDTH  : natural := 10    -- log2 of num_samples (rounded up)
+	NUM_SAMPLES : natural := 1024; -- When using RAM Maximum number of samples (must be < 1000000)
+	ADDR_WIDTH  : natural := 10    -- When using RAM log2 of num_samples (rounded up)
 );
 port(
 	data_in    : in  std_logic_vector(7 downto 0); -- input to be written
-	clk_out    : out std_logic; -- 48MHz clk for inter-procect use(module uses 1 of 1 HSOSC on chip)
+	clk_48     : in std_logic; -- 48MHz clk for inter-procect use(module uses 1 of 1 HSOSC on chip)
+	ext_clk    : in std_logic; -- Sampling clock to sync sampler with logic
+							   -- Must be manually connected, < 20 kHz and 'use_ext_clk' must be 1
 	serial_txd : out std_logic; -- UART Tx, must be pin 14
 	serial_rxd : in  std_logic; -- UART Rx, must be pin 15
 	spi_cs     : out std_logic  -- UART CS, must be pin 16 and HIGH
 );
 end;
 
-architecture synth of top is
+architecture synth of MNC is
 
-signal clk_48         : std_logic; -- 48 MHz clk
-signal ext_clk        : std_logic; -- Sampling clock to sync sampler with logic
-								   -- Must be manually connected, < 20 kHz and 'use_ext_clk' must be 1
 signal data_in_RAM    : std_logic_vector(7 downto 0);
 signal clk_48_RAM     : std_logic;
 signal serial_txd_RAM : std_logic;
@@ -40,14 +39,6 @@ signal serial_rxd_RAW : std_logic;
 
 signal test_counter : unsigned(30 downto 0);
 
-component HSOSC is
-generic (
-	CLKHF_DIV : String := "0b00"); -- Divide 48MHz clock by 2?N (0-3)
-port(
-	CLKHFPU : in std_logic := 'X'; -- Set to 1 to power up
-	CLKHFEN : in std_logic := 'X'; -- Set to 1 to enable output
-	CLKHF : out std_logic := 'X'); -- Clock output
-end component;
 
 -- RAM SAMPLER
 component ram_sampler is 
@@ -85,11 +76,7 @@ end component;
 
 
 begin
---generate 48MHz clock
-osc : HSOSC generic map ( CLKHF_DIV => "0b00")
-	port map (CLKHFPU => '1',
-	CLKHFEN => '1',
-	CLKHF => clk_48);
+
 
 ram_sample : ram_sampler 
 generic map (
@@ -120,17 +107,16 @@ generic map (
 
 
 -- Set top output signals to associated middle level signals
--- Dependant on sample_type alone
-data_in_RAM    <= data_in    when sample_type = 0 else "00000000"; 
-clk_48_RAM     <= clk_48     when sample_type = 0 else '0';
-serial_txd_RAM <= serial_txd when sample_type = 0 else '0';
-serial_rxd_RAM <= serial_rxd when sample_type = 0 else '0';
+-- Dependant on use_ram alone
+data_in_RAM    <= data_in    when use_ram = 1 else "00000000"; 
+clk_48_RAM     <= clk_48     when use_ram = 1 else '0';
+serial_rxd_RAM <= serial_rxd when use_ram = 1 else '0';
 
+data_in_RAW    <= data_in    when use_ram = 0 else "00000000";
+clk_48_RAW     <= clk_48     when use_ram = 0 else '0';
+serial_rxd_RAW <= serial_rxd when use_ram = 0 else '0';
 
-data_in_RAW    <= data_in    when sample_type = 1 else "00000000";
-clk_48_RAW     <= clk_48     when sample_type = 1 else '0';
-serial_txd_RAW <= serial_txd when sample_type = 1 else '0';
-serial_rxd_RAW <= serial_rxd when sample_type = 1 else '0';
+serial_txd <= serial_txd_RAW when use_ram = 0 else serial_txd_RAM;
 
 end;
 
